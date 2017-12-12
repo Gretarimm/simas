f2b03dd1693618c9ab9e329afdbd14507c7d4c60
Greta <gretarimm27@gmail.com>
2017-12-12 13:01:26 +0200

    su nav problema

---
+++ Simui/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  	<script
+    src="https://code.jquery.com/jquery-3.2.1.js"
+    integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+    crossorigin="anonymous"></script>
+ 	
+ 	<!-- <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/icons/icons_reference.asp"> -->
+	<title>Document</title>
+</head>
+<body>
+	<header>
+		<a href=""><img   src="img/logo3.png"></a>
+		<div class="toggle">
+			<i class="fa fa-bars menu" aria-hidden="true" ></i>
+		</div>
+		<nav id="myTopnav">
+			
+
+			
+			<!-- <label for="toggle">&#9776</label>
+   			<input type="checkbox" id="toggle"> -->
+			<ul>
+				<li><a href="#pirma">HOME</a></li>
+				<li><a href="#antra">SELL YOUR GOLD</a></li>
+				<li><a href="#trecia">FAQ</a></li>
+				<li><a href="#ketvirta">ABOUT US</a></li>
+				<li><a href="#penkta">TERMS</a></li>
+				<li><a href="#sesta">CONTACT</a></li>
+
+			</ul>
+		</nav>
+		<div>
+			<a href="#"><b>$</b>USD</a>
+			<a href="#"><b>€</b>EUR</a>
+			<a href="#"><b>£</b>GBP</a>
+		</div>	
+	</header>
+	 <section id="nuline">
+	 	<div class="wrapper">
+	 		<div class="lentelea">
+				<div class="pirma">
+					<div class="title">
+						<h1>You can buy Runescape<br><strong>OSRS</strong> Gold in Milions</h1>
+					</div>
+					<form action="/kazkas.php">
+						<div>
+							<h1>€ 0.85/M</h1>
+							<p>Please anter amount below to see the total price:</p>
+							<h2>€ 8.50</h2>
+							<p>Amount in Millions (1 for : 1 Milion)</p>
+						</div>
+
+						<input type="number" min="10" max="99999999" step="1" name="amount" placeholder="Number of Milions" value="10" reguired>
+						<a href="#" class="button">BUY NOW</a>
+					</form>
+				</div>
+				<div class="antra">
+					<div class="title">
+						<h1>You can buy Runescape<br><strong>RS3</strong> Gold in Milions</h1>
+					</div>
+					<form  action="/kazkas.php">
+						<div >
+							<h1>€ 0.14/M</h1>
+							<p>Please anter amount below to see the total price:</p>
+							<h2>€ 1.40</h2>
+							<p>Amount in Millions (1 for : 1 Milion)</p>
+						</div>
+
+						<input type="number" min="50" max="99999999" step="1" name="amount" placeholder="Number of Milions" value="50" reguired><br>
+						<a href="#" class="button">BUY NOW</a>
+					</form>
+				</div>	
+			</div>
+			<div style="clear:both;width: 100%;"></div>
+			l
+		</div>
+	</section>
+
+	
+	<section id="pirma">		
+	</section>
+	
+	<section id="antra">	
+	</section>
+
+	<section id="trecia">		
+	</section>
+	<script src="main.js">
+		
+	</script>
+</body>
+</html>
