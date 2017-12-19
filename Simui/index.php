47645e542ffa7986758b79d09c43c0cc9970601c
Greta <gretarimm27@gmail.com>
2017-12-19 10:09:23 +0200

    gatavas

--- Simui/index.php
+++ Simui/index.php
@@ -4,95 +4,198 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  	<script
+	<script
     src="https://code.jquery.com/jquery-3.2.1.js"
     integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
     crossorigin="anonymous"></script>
- 	
- 	<!-- <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/icons/icons_reference.asp"> -->
 	<title>Document</title>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+
 </head>
 <body>
 	<header>
-		<a href=""><img   src="img/logo3.png"></a>
-		<div class="toggle">
-			<i class="fa fa-bars menu" aria-hidden="true" ></i>
-		</div>
-		<nav id="myTopnav">
-			
-
-			
-			<!-- <label for="toggle">&#9776</label>
-   			<input type="checkbox" id="toggle"> -->
-			<ul>
-				<li><a href="#pirma">HOME</a></li>
-				<li><a href="#antra">SELL YOUR GOLD</a></li>
-				<li><a href="#trecia">FAQ</a></li>
-				<li><a href="#ketvirta">ABOUT US</a></li>
-				<li><a href="#penkta">TERMS</a></li>
-				<li><a href="#sesta">CONTACT</a></li>
-
-			</ul>
-		</nav>
-		<div>
-			<a href="#"><b>$</b>USD</a>
-			<a href="#"><b>€</b>EUR</a>
-			<a href="#"><b>£</b>GBP</a>
-		</div>	
+		<button class="hamburger">&#9776;</button>
+  		<button class="cross">&#735;</button>
+  			 <href="#nuline"><img src="img/logo3.png">
 	</header>
-	 <section id="nuline">
-	 	<div class="wrapper">
-	 		<div class="lentelea">
-				<div class="pirma">
-					<div class="title">
-						<h1>You can buy Runescape<br><strong>OSRS</strong> Gold in Milions</h1>
-					</div>
-					<form action="/kazkas.php">
-						<div>
-							<h1>€ 0.85/M</h1>
-							<p>Please anter amount below to see the total price:</p>
-							<h2>€ 8.50</h2>
-							<p>Amount in Millions (1 for : 1 Milion)</p>
-						</div>
-
-						<input type="number" min="10" max="99999999" step="1" name="amount" placeholder="Number of Milions" value="10" reguired>
-						<a href="#" class="button">BUY NOW</a>
+	<nav>
+		<a><img src="img/logo3.png"></a>
+		<ul>
+			<li><a href="#nuline">BUY YOUR GOLD</a></li>
+			<li><a href="#pirma">SELL YOUR GOLD</a></li>
+			<li><a href="#antra">FAQ</a></li>
+			<li><a href="#trecia">ABOUT ME</a></li>
+			<li><a href="#ketvirta">TERMS</a></li>
+			<li><a href="#penkta">CONTACT</a></li>
+		</ul>
+	</nav>
+	<section id="nuline" name="BUY GOLD">
+		<div class="wrapper">
+			<h1>BUY YOUR RUNESCAPE GOLD</h1>
+			<div>
+				<div class="plentele">
+					<h1>Buy Runescape<br><span>OSRS</span> Gold in Milions</h1>
+					<form>
+						<p>Amount in Millions (1 for : 1 Milion)</p>
+						<label for=""></label>
+						<input type="number" id="milai" placeholder="1 M"><br>
+						<p class="p">Minimum amount 10 Millions</p>
+						<label for="">The total price €:</label>
+						<input type="number" id="eurai" placeholder="€" ><br>
+						<label for="">The total price $:</label>
+						<input type="number" id="USD" placeholder="$"><br>
+						<label for="">The total price £:</label>
+						<input type="number" id="GBP" placeholder="£"><br>
+						<a href="https://www.facebook.com/simas.ivanovas" class="button" target="_blank">BUY NOW</a>
 					</form>
 				</div>
-				<div class="antra">
-					<div class="title">
-						<h1>You can buy Runescape<br><strong>RS3</strong> Gold in Milions</h1>
-					</div>
-					<form  action="/kazkas.php">
-						<div >
-							<h1>€ 0.14/M</h1>
-							<p>Please anter amount below to see the total price:</p>
-							<h2>€ 1.40</h2>
-							<p>Amount in Millions (1 for : 1 Milion)</p>
-						</div>
-
-						<input type="number" min="50" max="99999999" step="1" name="amount" placeholder="Number of Milions" value="50" reguired><br>
-						<a href="#" class="button">BUY NOW</a>
+				<div class="alentele">
+					<h1>Buy Runescape<br><span>RS3</span> Gold in Milions</h1>
+					<form>
+						<p>Amount in Millions (1 for : 1 Milion)</p>
+						<label for=""></label>
+						<input type="text" id="milaia" placeholder="1 M"><br>
+						<p class="p">Minimum amount 10 Millions</p>
+						<label for="">The total price €:</label>
+						<input type="text" id="euraia" placeholder="€"><br>
+						<label for="">The total price $:</label>
+						<input type="text" id="USDa" placeholder="$"><br>
+						<label for="">The total price £:</label>
+						<input type="text" id="GBPa" placeholder="£"><br>
+						<a href="https://www.facebook.com/simas.ivanovas" class="button" target="_blank">BUY NOW</a>
 					</form>
-				</div>	
+				</div>
 			</div>
-			<div style="clear:both;width: 100%;"></div>
-			l
 		</div>
 	</section>
 
-	
-	<section id="pirma">		
+	<section id="pirma" name="SEL GOLD">
+		<div class="wrapper">
+			<h1>SELL YOUR RUNESCAPE GOLD</h1>
+			<div>
+				<div class="palentele">
+					<h1>Sell your <span>OSRS</span> Gold</h1>
+					<form>
+						<p>Amount in Millions (1 for : 1 Milion)</p>
+						<label for=""></label>
+						<input type="text" id="milaib" placeholder="1 M"><br>
+						<label for="">The total price €:</label>
+						<input type="text" id="euraib" placeholder="€"><br>
+						<label for="">The total price $:</label>
+						<input type="text" id="USDb" placeholder="$"><br>
+						<label for="">The total price £:</label>
+						<input type="text" id="GBPb" placeholder="£"><br>
+						<a href="https://www.facebook.com/simas.ivanovas" class="button" target="_blank">BUY NOW</a>
+					</form>
+				</div>
+				<div class="aalentele">
+					<h1>Sell your <span>RS3</span> Gold</h1>
+					<form>
+						<p>Amount in Millions (1 for : 1 Milion)</p>
+						<label for=""></label>
+						<input type="text" id="milaic" placeholder="1 M"><br>
+						<label for="">The total price €:</label>
+						<input type="text" id="euraic" placeholder="€"><br>
+						<label for="">The total price $:</label>
+						<input type="text" id="USDc" placeholder="$"><br>
+						<label for="">The total price £:</label>
+						<input type="text" id="GBPc" placeholder="£"><br>
+						<a href="https://www.facebook.com/simas.ivanovas" class="button" target="_blank">BUY NOW</a>
+					</form>
+				</div>
+			</div>
+			
+		</div>
 	</section>
 	
-	<section id="antra">	
-	</section>
+	<section id="antra" name="FAQ">
+		<div class="wrapper">
+			<div>
+				<h1>FAQ</h1>
+				<div class="antraste">
+					<p><strong class="strong">Important! After you receive gold, never trade it back to anyone. Anybody who is claiming to work for us and who is asking for your gold back is a scammer!</strong></p>
 
-	<section id="trecia">		
-	</section>
-	<script src="main.js">
+					<p><strong class="strong">1. How do I buy Runescape gold?</strong><br>Just choose how much gold do you want to buy and pay for it using your PayPal account. Please keep in mind that you must be the owner of the PayPal account and the credit card that you have used for your purchase. Now you are ready to contact our 24/7 Live Support team and claim your Gold! Also note that you might be asked to submit a copy of your scanned ID to confirm your identity. If you cannot provide documents when you are asked to, you will be fully refunded.</p>
+
+					<p><strong class="strong">2. What payment methods do you accept for Runescape Gold?</strong><br>Feel free to order gold using PayPal at any time. We also accept Bitcoins, Bank wire transfers, Skrill (MoneyBookers) and Westen Union. If you want to use any payment method mentioned above, please contact our Live Support team.</p>
+
+					<p><strong class="strong">3. Will I need to provide my ID copy?</strong><br>In most cases we do ask for ID copy. This is to prevent buyers from using stolen PayPal accounts. We will accept any personal document issued by your government confirming your identity. Remember that your information in documents must match your PayPal account’s information. All your details will be kept safely and will only be disclosed in a case of fraud.</p>
+
+					<p><strong class="strong">4. What are chances that I will get banned?</strong><br>The possibility of getting banned is very small and it happens on very rare occasions. In fact, throughout our practice there has not been a single case when our buyer got banned for buying gold from us! However, if in an unfortunate situation it did happen, we would not take responsibility for Jagex decisions.</p>
+
+					<p><strong class="strong">5. When I will get my gold?</strong><br>95% of orders are delivered within 5 minutes. However, if due to some circumstances we were not able to deliver gold within 24 hours, you would be fully refunded. Fortunately, this has never occurred in our practice!</p>
+
+					<p><strong class="strong">6. How do I get my gold?</strong><br>After your payment gets processed, you need to contact our Live Support. You will be told a world name and a place where you can pick up your gold. Then, you will meet our trader in-game personally and trade him.</p>
+
+					<p><strong class="strong">7. Are you trusted?</strong><br>We have been in this business for more than 3 years. We have more than 10,000 satisfied customers and numbers are growing. We are happy to link you to websites where you can check our feedback!</p>
+
+					<p><strong class="strong">8. Additional information</strong><br>All trades are final. We do not own any items or gold in game. All content is property of Jagex. We only find buyers and sellers who want to exchange their real world money into in-game currency and vice versa. We reserve the right to refund orders if we believe that the purchase might be a fraud attempt. Use of VPN or Proxy is prohibited.</p>
 		
-	</script>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="trecia" name="ABOUT ME">
+		<div class="wrapper">
+				<h1>ABOUT ME</h1>
+				<p>
+					Food4Rs is a Runescape gold trading company based in Europe.
+				</p>
+				<p>
+					We have been in this business for more than 3 years now.
+				</p>
+				<p>
+					We started trading in various forums and with each year our staff became more and more professional.	
+				</p>
+				<p>
+					Our main goal is to provide the most convenient and efficient services while offering the best prices in the market.
+				</p>
+				<p>
+					With this approach and dedication to our work we have more than 10,000 satisfied customers and 100% positive feedback ratings.
+				</p>
+				<p>
+					 Also, there is a very helpful Live Support team which is always trying their very best to help you with any issue.
+				</p>
+				<p>
+					 We are online 24/7 and ready to buy or sell gold anytime!.
+				</p>
+		</div>
+	</section>
+	<section id="ketvirta" name="TERMS">
+		<div class="wrapper">
+			<h1>TERMS</h1>
+			<div>
+				<p><strong class="strong">1. TERMS AND CONDITIONS</strong><br>Food4rs.com is owned and regulated by MMO GAMES LP (Company). When visiting or using website you automatically accept our Terms and Conditions. Company have right to change Terms and Conditions at any time without notice. It is customer responsibility to be aware of Terms and review it regularly.</p>
+				<p><strong class="strong">2. GOODS DELIVERY</strong><br>After company receives payment from customer we establish fastest possible delivery to buyer using communication through live chat. All goods we sell are virtual goods and all sales are final. Company does not have returning policy regarding all sales are final.</p>
+				<p><strong class="strong">3. REFUND POLICY</strong><br>After purchase is complete and Company has received payment we establish delivery with buyer in shortest possible time. If delivery could not be established or customer wants full refund, we issue refund before goods delivery. Please note that refunds can take up to 1 week to complete. Refund are issued from Company side within 72 hours and it can take up to 4 working days to complete. This time period depends on payment method.</p>
+			</div>
+			
+		</div>
+	</section>
+	<section id="penkta" name="CONTACT">
+		<div class="wrapper">
+			<h1>CONTACT</h1>
+			<div>
+				<p>Click on Skype or Facebook icon to add</p>
+				<div class="facebook">
+					<a href="https://hatscripts.com/addskype/?tramadol422" target="_blank" ><img src="img/skype.png"></a>
+					<a href="https://www.facebook.com/simas.ivanovas" target="_blank"><img src="img/1-facebook.png"></a>
+				</div>
+			</div>
+			<p class="p">Pay methods</p>
+			<div class="pay-method">
+				<img src="img/paypal_logo1.jpg">
+				<img src="img/payments_320.jpg">
+			</div>
+			
+		</div>
+	</section>
+	<footer></footer>
+
+
+	<script src="main.js"></script>
+	
+	
 </body>
 </html>
--- Simui/main.js
+++ Simui/main.js
@@ -1,5 +1,269 @@
-$(document).ready(function(){
-		$(".menu").click(function(){
-				$("ul").tooggleClass(".active");
-	})
-})
+
+//apsirasau funkcija hamburgeriui
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+
+
+
+	$( ".hamburger" ).click(function() {
+		$( "nav" ).slideToggle( "slow", function() {
+			$( ".hamburger" ).hide();
+			$( ".cross" ).show();
+		});
+	});
+
+	$( ".cross" ).click(function() {
+		$( "nav" ).slideToggle( "slow", function() {
+			$( ".cross" ).hide();
+			$( ".hamburger" ).show();
+		});
+	});
+
+
+//    if($("nav").is(":visible")) {
+//             $( 'li' ).click( function() {
+//   $( 'nav').slideToggle('slow');
+//   $( ".hamburger" ).show();
+//   $( ".cross" ).hide();
+// });
+   // }
+   // 
+ var width = $(window).width();
+    if (width < 900) {
+        $( "li" ).click(function() {
+            $( "nav" ).slideToggle( "slow", function() {
+                $( ".hamburger" ).show();
+                $( ".cross" ).hide();
+            });
+        });
+    }
+
+    //  var mql = window.matchMedia("screen and (max-width: 900px)")
+    // if (mql.maches){
+    //     console.log("The condition " + mql.media + " has been met");
+    //     $( "li" ).click(function() {
+    //         $( "nav" ).slideToggle( "slow", function() {
+    //             $( ".hamburger" ).show();
+    //             $( ".cross" ).hide();
+    //         });
+    //     });
+    // }
+});
+
+
+// _____________________________________
+// Apsirasyti funkcijas valiutos konvertavimui
+function vienasMilijonasEurai(pinigai) {
+    var koef = 0.80;
+    return pinigai*koef;
+}
+function vienasMilijonasUSD(pinigai) {
+    var koefa = 0.90;
+    return pinigai*koefa;
+}
+function vienasMilijonasGBP(pinigai) {
+    var koefb = 0.74;
+    return pinigai*koefb;
+}
+function euraisGaunasi(pinigai) {
+    var koef = 0.80;
+    return pinigai/koef;
+}
+function USD(pinigai) {
+    var koefa = 0.90;
+    return pinigai/koefa;
+}
+function GBP(pinigai) {
+    var koefb = 0.74;
+    return pinigai/koefb;
+}
+$('#milai').on('keyup', function(){
+	var reiksmeInput =$('#milai').val();
+	var atsakymasEurai = vienasMilijonasEurai(reiksmeInput); 
+	var euruInput =$('#eurai').val(atsakymasEurai.toFixed(2));
+
+	var atsakymasUSD = vienasMilijonasUSD(reiksmeInput);
+	var usdInput =$('#USD').val(atsakymasUSD.toFixed(2));
+
+	var atsakymasGBP = vienasMilijonasGBP(reiksmeInput);
+	var gbpliuInput =$('#GBP').val(atsakymasGBP.toFixed(2));
+});
+$('#eurai').on('keyup', function(){
+    var reiksmeInput =$('#eurai').val();
+    var atsakymasEurai = euraisGaunasi(reiksmeInput);  
+    var EuruInput =$('#milai').val(atsakymasEurai.toFixed(2));
+});
+// ___________________
+$('#USD').on('keyup', function(){
+    var reiksmeInput =$('#USD').val();
+    var atsakymasUSD = USD(reiksmeInput);
+    var EuruInput =$('#milai').val(atsakymasUSD.toFixed(2));
+});
+//___________________________
+$('#GBP').on('keyup', function(){
+    var reiksmeInput =$('#GBP').val();
+    var atsakymasEurai = GBP(reiksmeInput);
+    var EuruInput =$('#milai').val(atsakymasEurai.toFixed(2));
+});
+// ______________antralentele________________________
+
+function vienasMilijonasEuraia(pinigai) {
+    var koef = 0.16;
+    return pinigai*koef;
+}
+function vienasMilijonasUSDa(pinigai) {
+    var koefa = 0.24;
+    return pinigai*koefa;
+}
+function vienasMilijonasGBPa(pinigai) {
+    var koefb = 0.13;
+    return pinigai*koefb;
+}
+function euraisGaunasia(pinigai) {
+    var koef = 0.16;
+    return pinigai/koef;
+}
+function USDa(pinigai) {
+    var koefa = 0.24;
+    return pinigai/koefa;
+}
+function GBPa(pinigai) {
+    var koefb = 0.13;
+    return pinigai/koefb;
+}
+$('#milaia').on('keyup', function(){
+    var reiksmeInputa =$('#milaia').val();
+    var atsakymasEuraia = vienasMilijonasEuraia(reiksmeInputa); 
+    var euruInputa =$('#euraia').val(atsakymasEuraia.toFixed(2));
+
+    var atsakymasUSDa = vienasMilijonasUSDa(reiksmeInputa);
+    var usdInputa =$('#USDa').val(atsakymasUSDa.toFixed(2));
+
+    var atsakymasGBPa = vienasMilijonasGBPa(reiksmeInputa);
+    var gbpliuInputa =$('#GBPa').val(atsakymasGBPa.toFixed(2));
+});
+$('#euraia').on('keyup', function(){
+    var reiksmeInputa =$('#euraia').val();
+    var atsakymasEuraia = euraisGaunasia(reiksmeInputa);  
+    var EuruInputa =$('#milaia').val(atsakymasEuraia.toFixed(2));
+});
+// ___________________
+$('#USDa').on('keyup', function(){
+    var reiksmeInputa =$('#USDa').val();
+    var atsakymasUSDa = USDa(reiksmeInputa);
+    var EuruInputa =$('#milaia').val(atsakymasUSDa.toFixed(2));
+});
+//___________________________
+$('#GBPa').on('keyup', function(){
+    var reiksmeInputa =$('#GBPa').val();
+    var atsakymasEuraia = GBPa(reiksmeInputa);
+    var EuruInputa =$('#milaia').val(atsakymasEuraia.toFixed(2));
+});
+
+//________kita sekcija______#pirma 1lentele____________
+function vienasMilijonasEuraib(pinigai) {
+    var koef = 0.16;
+    return pinigai*koef;
+}
+function vienasMilijonasUSDb(pinigai) {
+    var koefa = 0.24;
+    return pinigai*koefa;
+}
+function vienasMilijonasGBPb(pinigai) {
+    var koefb = 0.13;
+    return pinigai*koefb;
+}
+function euraisGaunasib(pinigai) {
+    var koef = 0.16;
+    return pinigai/koef;
+}
+function USDb(pinigai) {
+    var koefa = 0.24;
+    return pinigai/koefa;
+}
+function GBPb(pinigai) {
+    var koefb = 0.13;
+    return pinigai/koefb;
+}
+$('#milaib').on('keyup', function(){
+    var reiksmeInputb =$('#milaib').val();
+    var atsakymasEuraib = vienasMilijonasEuraia(reiksmeInputb); 
+    var euruInputb =$('#euraib').val(atsakymasEuraib.toFixed(2));
+
+    var atsakymasUSDb = vienasMilijonasUSDb(reiksmeInputb);
+    var usdInputb =$('#USDb').val(atsakymasUSDb.toFixed(2));
+
+    var atsakymasGBPb = vienasMilijonasGBPb(reiksmeInputb);
+    var gbpliuInputb =$('#GBPb').val(atsakymasGBPb.toFixed(2));
+});
+$('#euraib').on('keyup', function(){
+    var reiksmeInputb =$('#euraib').val();
+    var atsakymasEuraib = euraisGaunasib(reiksmeInputb);  
+    var EuruInputb =$('#milaib').val(atsakymasEuraib.toFixed(2));
+});
+// ___________________
+$('#USDb').on('keyup', function(){
+    var reiksmeInputb =$('#USDb').val();
+    var atsakymasUSDb = USDb(reiksmeInputb);
+    var EuruInputb =$('#milaib').val(atsakymasUSDb.toFixed(2));
+});
+//___________________________
+$('#GBPb').on('keyup', function(){
+    var reiksmeInputb =$('#GBPb').val();
+    var atsakymasEuraib = GBPa(reiksmeInputb);
+    var EuruInputb =$('#milaib').val(atsakymasEuraib.toFixed(2));
+});
+//____________________________________________
+function vienasMilijonasEuraic(pinigai) {
+    var koef = 0.16;
+    return pinigai*koef;
+}
+function vienasMilijonasUSDc(pinigai) {
+    var koefa = 0.24;
+    return pinigai*koefa;
+}
+function vienasMilijonasGBPc(pinigai) {
+    var koefb = 0.13;
+    return pinigai*koefb;
+}
+function euraisGaunasic(pinigai) {
+    var koef = 0.16;
+    return pinigai/koef;
+}
+function USDc(pinigai) {
+    var koefa = 0.24;
+    return pinigai/koefa;
+}
+function GBPc(pinigai) {
+    var koefb = 0.13;
+    return pinigai/koefb;
+}
+$('#milaic').on('keyup', function(){
+    var reiksmeInputc =$('#milaic').val();
+    var atsakymasEuraic = vienasMilijonasEuraic(reiksmeInputc); 
+    var euruInputc =$('#euraic').val(atsakymasEuraic.toFixed(2));
+
+    var atsakymasUSDc = vienasMilijonasUSDc(reiksmeInputc);
+    var usdInputc =$('#USDc').val(atsakymasUSDc.toFixed(2));
+
+    var atsakymasGBPc = vienasMilijonasGBPc(reiksmeInputc);
+    var gbpliuInputc =$('#GBPc').val(atsakymasGBPc.toFixed(2));
+});
+$('#euraic').on('keyup', function(){
+    var reiksmeInputc =$('#euraic').val();
+    var atsakymasEuraic = euraisGaunasic(reiksmeInputc);  
+    var EuruInputc =$('#milaic').val(atsakymasEuraic.toFixed(2));
+});
+// ___________________
+$('#USDc').on('keyup', function(){
+    var reiksmeInputc =$('#USDc').val();
+    var atsakymasUSDc = USDc(reiksmeInputc);
+    var EuruInputc =$('#milaic').val(atsakymasUSDc.toFixed(2));
+});
+//___________________________
+$('#GBPc').on('keyup', function(){
+    var reiksmeInputc =$('#GBPc').val();
+    var atsakymasEuraic = GBPa(reiksmeInputc);
+    var EuruInputc =$('#milaic').val(atsakymasEuraic.toFixed(2));
+});
